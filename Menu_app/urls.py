a699bc7ec44641e4a1edd77be23e75b64e657888
Bobcat997 <bszczypka978@gmail.com>
2022-09-30 23:10:57 +0200

    Add templates and crispy forms

--- Menu_app/urls.py
+++ Menu_app/urls.py
@@ -3,5 +3,7 @@ from menu_app import views
 
 urlpatterns = [
     path('menu_app/', views.index, name='index'),
+    path('menu_add/', views.menu_add, name="menu_add"),
+    path('dishes_add/', views.dishes_add, name="dishes_add")
 
 ]
