90d6484ae7417f9e12c340f5344693eb3b57741d
Bobcat997 <bszczypka978@gmail.com>
2022-10-14 11:48:35 +0200

    convert functions into classes

--- Menu_app/urls.py
+++ Menu_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from menu_app import views
 
 urlpatterns = [
-    path('menu_app/', views.index, name='index'),
-    path('menu_add/', views.menu_add, name="menu_add"),
-    path('dishes_add/', views.dishes_add, name="dishes_add"),
-    path('delete/<int:id>/', views.DeleteMenu, name='DeleteMenu'),
-    path('<int:id>/', views.UpdateMenu, name='UpdateMenu'),
+    path('menu_app/', views.index.as_view(), name='index'),
+    path('menu_add/', views.MenuAdd.as_view(), name="menu_add"),
+    path('dishes_add/', views.DishesAdd.as_view(), name="dishes_add"),
+    path('delete/<int:pk>/', views.DeleteMenu.as_view(), name='DeleteMenu'),
+    path('<int:pk>/', views.UpdateMenu.as_view(), name='UpdateMenu'),
 ]
--- Menu_app/views.py
+++ Menu_app/views.py
@@ -1,72 +1,103 @@
 from django.shortcuts import render, redirect
 from menu_app import models
 from .forms import DishesForm, MenuForm
-from django.contrib import messages
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import UpdateView, DeleteView, CreateView, ListView
+from django.urls import reverse
+
+# old approach
+
+# def index(request):
+#     if request.method == 'GET':
+#         query = request.GET.get('query')
+#         if query:
+#             menus = models.Menu.objects.filter(name__icontains=query)
+#         else:
+#             menus = models.Menu.objects.all()
+#         return render(request, 'index.html', {"menus": menus, })
 
 
-def index(request):
-    if request.method == 'GET':
-        query = request.GET.get('query')
-        if query:
-            menus = models.Menu.objects.filter(name=query)
-        else:
-            menus = models.Menu.objects.all()
-        return render(request, 'index.html', {"menus": menus, })
+# def menu_add(request):
+#     form = MenuForm()
+
+#     if request.method == 'POST':
+#         form = MenuForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Menu added")
+
+#     context = {'form': form}
+#     return render(request, 'menu_add.html', context)
 
 
-def menu_add(request):
-    form = MenuForm()
+# def dishes_add(request):
+#     form = DishesForm()
 
-    if request.method == 'POST':
-        form = MenuForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Menu added")
+#     if request.method == 'POST':
+#         form = DishesForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Dishes added")
 
-    context = {'form': form}
-    return render(request, 'menu_add.html', context)
-
-
-def dishes_add(request):
-    form = DishesForm()
-
-    if request.method == 'POST':
-        form = DishesForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Dishes added")
-
-    context = {'form': form}
-    return render(request, 'dishes_add.html', context)
+#     context = {'form': form}
+#     return render(request, 'dishes_add.html', context)
 
 
-def DeleteMenu(request, id):
-    menu = models.Menu.objects.get(pk=id)
-    menu.delete()
-    return redirect('/menu_app')
+# def DeleteMenu(request, id):
+#     menu = models.Menu.objects.get(pk=id)
+#     menu.delete()
+#     return redirect('/menu_app')
 
 
-def UpdateMenu(request, id=0):
-    if request.method == "GET":
-        if id == 0:
-            form = MenuForm()
-        else:
-            menu = models.Menu.objects.get(pk=id)
-            form = MenuForm(instance=menu)
-        return render(request, "menu_add.html", {'form': form})
-    else:
-        if id == 0:
-            form = MenuForm(request.POST)
-        else:
-            menu = models.Menu.objects.get(pk=id)
-            form = MenuForm(request.POST, instance=menu)
-        if form.is_valid():
-            form.save()
-        return redirect('/menu_app')
+# def UpdateMenu(request, id=0):
+#     if request.method == "GET":
+#         if id == 0:
+#             form = MenuForm()
+#         else:
+#             menu = models.Menu.objects.get(pk=id)
+#             form = MenuForm(instance=menu)
+#         return render(request, "menu_add.html", {'form': form})
+#     else:
+#         if id == 0:
+#             form = MenuForm(request.POST)
+#         else:
+#             menu = models.Menu.objects.get(pk=id)
+#             form = MenuForm(request.POST, instance=menu)
+#         if form.is_valid():
+#             form.save()
+#         return redirect('/menu_app')
 
 
-class MenuListView(ListView):
-    paginate_by = 2
+class index(ListView):
     queryset = models.Menu.objects.all()
+    context_object_name = 'menus'
     template_name = 'index.html'
+
+
+class DishesAdd(SuccessMessageMixin, CreateView):
+    model = models.Menu
+    form_class = DishesForm
+    template_name = 'dishes_add.html'
+    success_url = '/dishes_add/'
+    success_message = 'Dishes Added'
+
+
+class MenuAdd(SuccessMessageMixin, CreateView):
+    model = models.Menu
+    form_class = MenuForm
+    template_name = 'menu_add.html'
+    success_url = '/menu_add/'
+    success_message = 'Menu Added'
+
+
+class DeleteMenu(DeleteView):
+    model = models.Menu
+    template_name = "index.html"
+    success_url = '/menu_app/'
+
+
+class UpdateMenu(UpdateView):
+    queryset = models.Menu.objects.all()
+    template_name = 'menu_add.html'
+    form_class = MenuForm
+    success_url = '/menu_app/'
